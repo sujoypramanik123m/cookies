c0f959db299d4b1d1fd9873aad4bb926c3e1f88e
sujoypramanik123m <sujoypramanik123m@gmail.com>
2025-06-15 20:47:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ class Config(object):
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "7863236360:AAEY4TX71CqK664bIx7b3S8xpswB-0q3y2s")
     DB_URL = os.environ.get("DB_URL", "mongodb+srv://sujoy123m:wTWKGUaxYE7dxb1l@cluster0.zorxb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     DB_NAME = os.environ.get("DB_NAME", "video_compressor")
-    ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get("ADMIN", "7095634320").split()]
+    ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get("ADMIN", "8181241262").split()]
     LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002872961182"))
     BOT_UPTIME = time.time()
     MAX_QUEUE_SIZE = 5
